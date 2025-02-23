6fb5f76fc1c9198adad3ce344ea31900588a644e
armaanc <armaan.c2009@gmail.com>
2025-02-23 12:18:33 -0600

    rename node to ll_node in case other data structures have nodes

--- main/src/linkedlist.h
+++ main/src/linkedlist.h
@@ -2,36 +2,36 @@
 
 #include <stdlib.h>
 
-struct node
+struct ll_node
 {
     int value;
-    struct node* next;
+    struct ll_node* next;
 };
 // weird syntax but necessary for self-referential struct
-typedef struct node node;
+typedef struct ll_node ll_node;
 
 typedef struct 
 {
-    node* head;
+    ll_node* head;
 } linked_list;
 
 void ll_init(linked_list* ll_in, int head_val)
 {
-    node* head_node = (node*)malloc(sizeof(node));
+    ll_node* head_node = (ll_node*)malloc(sizeof(ll_node));
     head_node->value = head_val;
     ll_in->head = head_node;
 }
 
 void ll_push(linked_list* ll_in, int val)
 {
-    node* tracker = ll_in->head;
+    ll_node* tracker = ll_in->head;
 
     while (tracker->next != NULL)
     {
         tracker = tracker->next;
     }
 
-    node* new_node = (node*)malloc(sizeof(node));
+    ll_node* new_node = (ll_node*)malloc(sizeof(ll_node));
     new_node->value = val;
     tracker->next = new_node;
 }
@@ -39,7 +39,7 @@ void ll_push(linked_list* ll_in, int val)
 // returns the value of the popped node.
 int ll_pop(linked_list* ll_in)
 {
-    node* tracker = ll_in->head;
+    ll_node* tracker = ll_in->head;
     while (tracker->next->next != NULL)
     {
         tracker = tracker->next;
@@ -54,11 +54,11 @@ int ll_pop(linked_list* ll_in)
 // ll_in will NOT become null after this, but all the nodes will.
 void ll_free_nodes(linked_list* ll_in)
 {
-    node* tracker = ll_in->head;
+    ll_node* tracker = ll_in->head;
 
     while (tracker->next != NULL)
     {
-        node* old = tracker;
+        ll_node* old = tracker;
         tracker = tracker->next;
         old->next = NULL; // may not be necessary
         free(old);
